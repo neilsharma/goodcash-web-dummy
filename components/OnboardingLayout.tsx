67533740738ffe462f786bc2b0293359ee2080e7
Serhii Mamedov <bboyvivus@gmail.com>
2023-02-02 19:19:23 +0200

    Update layout with goodcash icon

--- components/OnboardingLayout.tsx
+++ components/OnboardingLayout.tsx
@@ -1,12 +1,20 @@
+import Image from "next/image";
 import type { FC, ReactNode } from "react";
 
-export const OnboardingLayout: FC<{ children: ReactNode }> = ({ children }) => {
+export const OnboardingLayout: FC<{ children?: ReactNode }> = ({
+  children,
+}) => {
   return (
     <>
-      <div className="flex justify-center mt-7 mb-24">
-        <span className="font-kansasNewSemiBold text-xl">GoodCash</span>
+      <div className="flex justify-center mt-8 mb-24">
+        <Image
+          src="/img/logo/goodcash.svg"
+          alt="GoodCash"
+          width={106}
+          height={16}
+        />
       </div>
-      <main className="max-w-screen-sm m-auto">{children}</main>
+      <main className="max-w-[500px] m-auto">{children}</main>
     </>
   );
 };
