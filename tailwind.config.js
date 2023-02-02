8edef1fbd3b75b4f2637e53938221053f9bee2b3
Serhii Mamedov <bboyvivus@gmail.com>
2023-02-02 19:19:39 +0200

    Register new fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,14 +8,17 @@ module.exports = {
     extend: {
       colors: {
         primary: "#296B4A",
-        "bg-primary": "#F1F4EA",
-        text: "#0F100F",
+        bgPrimary: "#F1F4EA",
+        text: "#333836",
+        boldText: "#0F100F",
       },
       fontFamily: {
         kansasNew: ["KansasNew-Regular"],
         kansasNewMedium: ["KansasNew-Medium"],
         kansasNewSemiBold: ["KansasNew-SemiBold"],
         kansasNewBold: ["KansasNew-Bold"],
+        sharpGroteskBook: ["SharpGrotesk-Book"],
+        sharpGroteskMedium: ["SharpGrotesk-Medium"],
       },
     },
   },
