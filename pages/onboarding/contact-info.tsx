4256684b1f2c874e7786f9ca986a280a5d5e1dba
Serhii Mamedov <bboyvivus@gmail.com>
2023-02-03 17:43:46 +0200

    Setup contact info page

--- pages/onboarding/contact-info.tsx
+++ pages/onboarding/contact-info.tsx
@@ -1,5 +1,17 @@
 import OnboardingLayout from "@/components/OnboardingLayout";
+import {
+  redirectIfServerSideRendered,
+  useConfirmUnload,
+  useContactInfoGuard,
+} from "@/shared/hooks";
 
 export default function OnboardingContactInfoPage() {
+  useConfirmUnload();
+  const allowed = useContactInfoGuard();
+
+  if (!allowed) return <OnboardingLayout />;
+
   return <OnboardingLayout>Contact Info</OnboardingLayout>;
 }
+
+export const getServerSideProps = redirectIfServerSideRendered;
