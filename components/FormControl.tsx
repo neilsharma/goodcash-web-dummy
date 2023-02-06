09b020a911b92281b7fb701d5a9a169665ac4bd6
Serhii Mamedov <bboyvivus@gmail.com>
2023-02-06 13:44:03 +0200

    Add description and label description to form control

--- components/FormControl.tsx
+++ components/FormControl.tsx
@@ -1,34 +1,42 @@
-import type { DetailedHTMLProps, FC, HTMLAttributes, InputHTMLAttributes } from "react";
+import type { DetailedHTMLProps, FC, InputHTMLAttributes, LabelHTMLAttributes } from "react";
 import { twMerge } from "tailwind-merge";
 
 export interface FormControlProps
   extends DetailedHTMLProps<InputHTMLAttributes<HTMLInputElement>, HTMLInputElement> {
   label?: string;
-  containerProps?: DetailedHTMLProps<HTMLAttributes<HTMLDivElement>, HTMLDivElement>;
+  labelDescription?: string;
+  description?: string;
+  containerProps?: DetailedHTMLProps<LabelHTMLAttributes<HTMLLabelElement>, HTMLLabelElement>;
 }
 
 export const FormControl: FC<FormControlProps> = ({
   label,
+  labelDescription,
+  description,
   containerProps,
   className,
   ...props
 }) => {
   return (
-    <div {...containerProps} className={twMerge("w-full my-7", containerProps?.className)}>
-      <label>
-        {label && (
-          <span className="my-3 block text-text font-sharpGroteskMedium text-sm">{label}</span>
-        )}
-        <input
-          className={twMerge(
-            "w-full p-4 rounded-lg border border-black/20 font-sharpGroteskBook",
-            className
+    <label {...containerProps} className={twMerge("w-full my-7 block", containerProps?.className)}>
+      {label && (
+        <div className="my-3 flex justify-between">
+          <span className="text-text font-sharpGroteskMedium text-sm">{label}</span>
+          {labelDescription && (
+            <span className="text-xs text-thinText sm:text-sm">{labelDescription}</span>
           )}
-          type="text"
-          {...props}
-        />
-      </label>
-    </div>
+        </div>
+      )}
+      <input
+        className={twMerge(
+          "w-full p-4 rounded-lg border border-black/20 font-sharpGroteskBook",
+          className
+        )}
+        type="text"
+        {...props}
+      />
+      {description && <span className="block mt-3 text-sm text-thinText">{description}</span>}
+    </label>
   );
 };
 
