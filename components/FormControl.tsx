a968a1ff9067db5ead001fd13e52f50d93ed3e5b
Serhii Mamedov <bboyvivus@gmail.com>
2023-02-06 16:55:41 +0200

    Add mask and error message to form control

--- components/FormControl.tsx
+++ components/FormControl.tsx
@@ -1,11 +1,22 @@
-import type { DetailedHTMLProps, FC, InputHTMLAttributes, LabelHTMLAttributes } from "react";
+import {
+  DetailedHTMLProps,
+  FC,
+  InputHTMLAttributes,
+  LabelHTMLAttributes,
+  createElement,
+} from "react";
+import Image from "next/image";
+import InputMask from "react-input-mask";
 import { twMerge } from "tailwind-merge";
 
-export interface FormControlProps
-  extends DetailedHTMLProps<InputHTMLAttributes<HTMLInputElement>, HTMLInputElement> {
+type InputProps = DetailedHTMLProps<InputHTMLAttributes<HTMLInputElement>, HTMLInputElement>;
+
+export interface FormControlProps extends InputProps {
   label?: string;
   labelDescription?: string;
   description?: string;
+  inputMask?: string;
+  error?: boolean | string | null;
   containerProps?: DetailedHTMLProps<LabelHTMLAttributes<HTMLLabelElement>, HTMLLabelElement>;
 }
 
@@ -15,8 +26,24 @@ export const FormControl: FC<FormControlProps> = ({
   description,
   containerProps,
   className,
+  inputMask,
+  error,
   ...props
 }) => {
+  const isError = !!error || typeof error === "string";
+
+  const inputProps: InputProps & Record<string, any> = {
+    className: twMerge(
+      `w-full p-4 rounded-lg font-sharpGroteskBook border border-${
+        isError ? "error" : "black/20"
+      } outline-${isError ? "error" : "inherit"}`,
+      className
+    ),
+    mask: inputMask,
+    type: "text",
+    ...props,
+  };
+
   return (
     <label {...containerProps} className={twMerge("w-full my-7 block", containerProps?.className)}>
       {label && (
@@ -27,15 +54,14 @@ export const FormControl: FC<FormControlProps> = ({
           )}
         </div>
       )}
-      <input
-        className={twMerge(
-          "w-full p-4 rounded-lg border border-black/20 font-sharpGroteskBook",
-          className
-        )}
-        type="text"
-        {...props}
-      />
+      {createElement(inputMask ? InputMask : "input", inputProps as any)}
       {description && <span className="block mt-3 text-sm text-thinText">{description}</span>}
+      {typeof error === "string" && (
+        <div className="flex mt-2 gap-2 items-end">
+          <Image src="/img/logo/alert.svg" alt="alert" width={24} height={24} priority={true} />
+          <span className="block text-sm text-error">{error}</span>
+        </div>
+      )}
     </label>
   );
 };
