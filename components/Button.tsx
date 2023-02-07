59357d4bd7e8fc3b3c5f6f0e2f6df99f74ccd536
Serhii Mamedov <bboyvivus@gmail.com>
2023-02-07 17:48:57 +0200

    Add variants of button component

--- components/Button.tsx
+++ components/Button.tsx
@@ -2,13 +2,17 @@ import type { ButtonHTMLAttributes, DetailedHTMLProps, FC } from "react";
 import { twMerge } from "tailwind-merge";
 
 export interface ButtonProps
-  extends DetailedHTMLProps<ButtonHTMLAttributes<HTMLButtonElement>, HTMLButtonElement> {}
+  extends DetailedHTMLProps<ButtonHTMLAttributes<HTMLButtonElement>, HTMLButtonElement> {
+  variant?: "contained" | "text";
+}
 
-export const Button: FC<ButtonProps> = ({ className, ...props }) => {
+export const Button: FC<ButtonProps> = ({ className, variant = "contained", ...props }) => {
   return (
     <button
       className={twMerge(
-        "w-full rounded-lg bg-primary text-white font-kansasNewMedium p-3 disabled:bg-gray-400 disabled:cursor-not-allowed disabled:text-gray-200 block",
+        "w-full rounded-lg font-kansasNewMedium p-3 disabled:bg-gray-400 disabled:cursor-not-allowed disabled:text-gray-200 block",
+        variant === "contained" ? "bg-primary" : "bg-transparent",
+        variant === "contained" ? "text-white" : "text-primary",
         className
       )}
       {...props}
