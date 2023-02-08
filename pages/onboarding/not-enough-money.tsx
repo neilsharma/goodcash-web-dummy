99c71478503788db7b429432ac901c4b2474f6d0
Serhii Mamedov <bboyvivus@gmail.com>
2023-02-08 14:36:07 +0200

    Remove unnecessary page

--- pages/onboarding/not-enough-money.tsx
+++
@@ -1,5 +0,0 @@
-import OnboardingLayout from "@/components/OnboardingLayout";
-
-export default function OnboardingNotEnoughMoneyPage() {
-  return <OnboardingLayout>Not enough money</OnboardingLayout>;
-}
