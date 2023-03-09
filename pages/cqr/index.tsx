7acb87f5a31be1225db90b2e91a29cdb4ac40522
Divyanshu Negi <divyanshunegi.dn@gmail.com>
2023-03-09 14:15:09 +0530

    fix: cqr page created with redirection logic

---
+++ pages/cqr/index.tsx
@@ -0,0 +1,27 @@
+import React from "react";
+import { GoodcashEnvironment } from "../../shared/config";
+
+const CQRPage = () => {
+  return (
+    <div className="flex flex-col justify-center items-center h-screen w-screen">
+      <h1 className=" font-kansasNewSemiBold text-4xl">Welcome to goodcash</h1>
+      <p className=" font-sharpGroteskBook mt-10 text-lg">Redirecting to Goodcash App</p>
+      <button
+        onClick={() => {
+          if (
+            (process.env["NEXT_PUBLIC_GOODCASH_ENVIRONMENT"] as GoodcashEnvironment) === "sandbox"
+          ) {
+            document.location = "goodcashsandbox://home";
+          } else {
+            document.location = "goodcash://home";
+          }
+        }}
+        className="bg-[#6BC799] p-4 rounded-lg text-white font- mt-4"
+      >
+        Click to redirect
+      </button>
+    </div>
+  );
+};
+
+export default CQRPage;
