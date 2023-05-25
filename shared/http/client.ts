4bdfff7a451a30c739b00fdfeaf36aa8d7a9747f
Divyanshu Negi <divyanshunegi.dn@gmail.com>
2023-05-25 20:53:36 +0800

    🚧  analytics events when state is rejected (#70)
    * fix: testing coverage call (#69)
    
    * fix: testing coverage call
    
    * fix: error logs
    
    * fix: added coverage path to unauthorised list
    
    * fix: updated the event name for state validation
    
    * fix: waitlist moved from types to constant
    
    * fix: code change

--- shared/http/client.ts
+++ shared/http/client.ts
@@ -8,24 +8,30 @@ export const http = axios.create({
   timeout: 10_000,
 });
 
-const unauthorizedRequests = () => {
-  return [urlPaths.USER_ME_CREATE, urlPaths.USER_STATE_COVERAGE];
-};
+// List of URL paths that do not require authentication for making requests
+const unauthorizedRequests = [urlPaths.USER_ME_CREATE, urlPaths.USER_STATE_COVERAGE];
 
+// Intercept requests and add authentication headers if necessary
 http.interceptors.request.use(async (config) => {
-  if (unauthorizedRequests().some((path) => config.url?.endsWith(path))) {
-    return config;
+  // Check if the request URL matches any of the unauthorized paths
+  if (unauthorizedRequests.some((path) => config.url?.endsWith(path))) {
+    return config; // Skip authentication for unauthorized requests
   }
 
   const controller = new AbortController();
   const auth = getComputedAuth();
   const token = await auth?.currentUser?.getIdToken();
 
-  if (!auth?.currentUser || !token) controller.abort();
+  // If there is no authenticated user or token, abort the request
+  if (!auth?.currentUser || !token) {
+    controller.abort();
+  }
 
+  // Add the authentication token to the request headers
   config.headers = config.headers ?? {};
   config.headers["goodcash-authorization"] ??= token;
 
+  // Include the signal from the AbortController in the request configuration
   return { ...config, signal: controller.signal };
 });
 
